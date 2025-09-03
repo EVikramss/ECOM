8f186fed19aed47d82336a1c79e4406d4b2784e8
Vikram <evikramss@gmail.com>
2025-09-03 09:51:28 +0000

    schedule order updates 2

--- modules/OrderManagementModule/ScheduleOrder/src/main/java/com/schedule/Init.java
+++ modules/OrderManagementModule/ScheduleOrder/src/main/java/com/schedule/Init.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Init {
 
 	public static void main(String[] args) {
+System.out.println(System.getenv("SECRET"));
 		SpringApplication.run(Init.class, args);
 	}
 }
