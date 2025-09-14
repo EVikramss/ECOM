8508af3fe91e3ed4dfb05443055e9956a1fd46e8
Vikram <evikramss@gmail.com>
2025-09-14 07:23:53 +0000

    create order updates

--- modules/OrderManagementModule/CreateOrder/src/main/java/com/ecom/services/QueueService.java
+++ modules/OrderManagementModule/CreateOrder/src/main/java/com/ecom/services/QueueService.java
@@ -27,85 +27,85 @@ import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
 @Service
 public class QueueService {
 
-  private static final Logger LOGGER = LogManager.getLogger(QueueService.class);
+	private static final Logger LOGGER = LogManager.getLogger(QueueService.class);
 
-  @Autowired
-  private SqsClient sqsClient;
+	@Autowired
+	private SqsClient sqsClient;
 
-  @Autowired
-  private OrderService orderService;
+	@Autowired
+	private OrderService orderService;
 
-  @Autowired
-  private Validator validator;
+	@Autowired
+	private Validator validator;
 
-  @Autowired
-  private OrderDataMapper orderDataMapper;
+	@Autowired
+	private OrderDataMapper orderDataMapper;
 
-  @Autowired
-  private ErrorService errorService;
+	@Autowired
+	private ErrorService errorService;
 
-  @Value("${CreateOrderQURL}")
-  private String queueUrl;
+	@Value("${CreateOrderQURL}")
+	private String queueUrl;
 
-  private final ExecutorService executorService = Executors.newFixedThreadPool(2);
+	private final ExecutorService executorService = Executors.newFixedThreadPool(2);
 
-  @PostConstruct
-  public void startPolling() {
-    for (int i = 0; i < 2; i++) {
-      executorService.submit(() -> pollMessages());
-    }
-  }
+	@PostConstruct
+	public void startPolling() {
+		for (int i = 0; i < 2; i++) {
+			executorService.submit(() -> pollMessages());
+		}
+	}
 
-  private void pollMessages() {
-    LOGGER.info("Listening on queue " + queueUrl + " with thread " + Thread.currentThread().getName());
+	private void pollMessages() {
+		LOGGER.info("Listening on queue " + queueUrl + " with thread " + Thread.currentThread().getName());
 
-    while (true) {
-      try {
-        ReceiveMessageRequest request = ReceiveMessageRequest.builder().queueUrl(queueUrl).waitTimeSeconds(20)
-            .maxNumberOfMessages(10).build();
+		while (true) {
+			try {
+				ReceiveMessageRequest request = ReceiveMessageRequest.builder().queueUrl(queueUrl).waitTimeSeconds(20)
+						.maxNumberOfMessages(10).build();
 
-        List<Message> messages = sqsClient.receiveMessage(request).messages();
+				List<Message> messages = sqsClient.receiveMessage(request).messages();
 
-        for (Message message : messages) {
-          processMessage(message);
-          deleteMessage(message);
-        }
-      } catch (Exception e) {
-        // in case of unhandled exception from processMessage, message is not deleted
-        // from q. Let the exception stop the thread, instead of polling same message again and
-        // going into a loop
-        e.printStackTrace();
-        throw e;
-      }
-    }
-  }
+				for (Message message : messages) {
+					processMessage(message);
+					deleteMessage(message);
+				}
+			} catch (Exception e) {
+				// in case of unhandled exception from processMessage, message is not deleted
+				// from q. Let the exception stop the thread, instead of polling same message again and
+				// going into a loop
+				e.printStackTrace();
+				throw e;
+			}
+		}
+	}
 
-  private void processMessage(Message message) {
-    String msgBody = message.body();
-    processQMessage(msgBody);
-  }
+	private void processMessage(Message message) {
+		String msgBody = message.body();
+		processQMessage(msgBody);
+	}
 
-  public void processQMessage(String msgBody) {
-    ObjectMapper mapper = new ObjectMapper();
-    try {
-      com.ecom.dto.OrderData inputData = mapper.readValue(msgBody, com.ecom.dto.OrderData.class);
-      Set<ConstraintViolation<com.ecom.dto.OrderData>> violations = validator.validate(inputData);
-      if (!violations.isEmpty()) {
-        throw new ConstraintViolationException(violations);
-      }
-      orderService.createOrder(orderDataMapper.convertFromDTO(inputData));
-    } catch (ServiceException e) {
-      e.printStackTrace();
-      errorService.persistData(e);
-    } catch (Exception e) {
-      e.printStackTrace();
-      errorService.persistData(e, msgBody);
-    }
-  }
+	public void processQMessage(String msgBody) {
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			com.ecom.dto.OrderData inputData = mapper.readValue(msgBody, com.ecom.dto.OrderData.class);
+			Set<ConstraintViolation<com.ecom.dto.OrderData>> violations = validator.validate(inputData);
+			if (!violations.isEmpty()) {
+				throw new ConstraintViolationException(violations);
+			}
+			orderService.createOrder(orderDataMapper.convertFromDTO(inputData));
+		} catch (ServiceException e) {
+			e.printStackTrace();
+			errorService.persistData(e);
+		} catch (Exception e) {
+			e.printStackTrace();
+			errorService.persistData(e, msgBody);
+		}
+	}
 
-  private void deleteMessage(Message message) {
-    DeleteMessageRequest deleteRequest = DeleteMessageRequest.builder().queueUrl(queueUrl)
-        .receiptHandle(message.receiptHandle()).build();
-    sqsClient.deleteMessage(deleteRequest);
-  }
+	private void deleteMessage(Message message) {
+		DeleteMessageRequest deleteRequest = DeleteMessageRequest.builder().queueUrl(queueUrl)
+				.receiptHandle(message.receiptHandle()).build();
+		sqsClient.deleteMessage(deleteRequest);
+	}
 }
