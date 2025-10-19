eeeedbf4c1b5a53ca71dd1d57a259f51e94521a9
Vikram <evikramss@gmail.com>
2025-10-19 14:52:06 +0000

    order capture updates

--- modules/OrderCaptureModule/_scripts/deploy.sh
+++ modules/OrderCaptureModule/_scripts/deploy.sh
@@ -6,12 +6,14 @@ export OrderHistoryReservedConcurrency=$3
 export OrderHistoryProvisionedConcurrency=$4
 export initialSkuCount=$5
 
-# create avl db.
-. ./createAvlDB.sh
 
 # deploy stack
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $SCRIPT_DIR
+
+# create avl db.
+. ./createAvlDB.sh
+
 . ./checkForExistingResources.sh
 
 cd $SCRIPT_DIR/../../../cdks/EcomOrdCptCDK
