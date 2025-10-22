4a7423c292cfb3e3c9f24964218553e67b0f3241
Vikram <evikramss@gmail.com>
2025-10-22 11:07:01 +0000

    deploymennt script update

--- modules/OrderCaptureModule/Database/_scripts/checkExisting.sh
+++ modules/OrderCaptureModule/Database/_scripts/checkExisting.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if aws dynamodb describe-table --table-name ItemInfo --region us-east-1 > /dev/null 2>&1; then
-    echo "ItemInfo dynamodb exists."
+if aws dynamodb describe-table --table-name UserInfo --region us-east-1 > /dev/null 2>&1; then
+    echo "UserInfo dynamodb exists."
 	exit 1
 else
-    echo "ItemInfo dynamodb does not exist."
+    echo "UserInfo dynamodb does not exist."
 	exit 0
 fi
