9893510044c0e7f87873a7dde28af0ceb97d2b87
Vikram <evikramss@gmail.com>
2025-10-24 07:09:26 +0000

    add inventory for sample ite.s

--- modules/InventoryModule/_scripts/postDeploy.sh
+++ modules/InventoryModule/_scripts/postDeploy.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+# To be removed once s3 inventory load is conplete
+aws lambda invoke --function-name ECOMINVSupplyDemandOp --payload '{"OP":"UpdateSupply","ItemID":"sku0","Node":"node1","Qty":10,"SupplyType":"Current","SupplyDate":"2025-10-24","Override":"Y"}' --cli-binary-format raw-in-base64-out response.json 
+
+aws lambda invoke --function-name ECOMINVSupplyDemandOp --payload '{"OP":"UpdateSupply","ItemID":"sku1","Node":"node1","Qty":10,"SupplyType":"Current","SupplyDate":"2025-10-24","Override":"Y"}' --cli-binary-format raw-in-base64-out response.json 
+
+aws lambda invoke --function-name ECOMINVSupplyDemandOp --payload '{"OP":"UpdateSupply","ItemID":"sku2","Node":"node1","Qty":10,"SupplyType":"Current","SupplyDate":"2025-10-24","Override":"Y"}' --cli-binary-format raw-in-base64-out response.json
